b90b22c59852d3edb27f7fcb3272eeae30492678
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-07-09 23:19:05 +0100

    Add --silent option to curl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-curl -L https://github.com/kowainik/stan/releases/latest/download/stan-0.0.1.0-Linux-ghc-8.8.3 --output stan
+curl --silent -L https://github.com/kowainik/stan/releases/latest/download/stan-0.0.1.0-Linux-ghc-8.8.3 --output stan
 echo "::add-path::$(pwd)/stan"
