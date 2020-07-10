91e6748ffac66c1d5f1cb873eeb5cd2f6a5bfece
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-07-10 20:02:18 +0100

    Temp debug commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 
+set -euxo pipefail
+
 curl --silent -L https://github.com/kowainik/stan/releases/latest/download/stan-0.0.1.0-Linux-ghc-8.8.3 --output stan
-echo "::add-path::$(pwd)/stan"
+
+echo "Pwd: $(pwd)"
+echo "::set-output name=stanPath::$(pwd)/stan"
